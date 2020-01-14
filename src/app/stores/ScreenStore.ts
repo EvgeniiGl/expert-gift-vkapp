4de9c2f34e8f73a3df5c53dfaa87696a52d1e689
Evgehii <geka_nkz@mail.ru>
2020-01-14 13:53:21 +0700

    deploy gh-pages

--- src/app/stores/ScreenStore.ts
+++ src/app/stores/ScreenStore.ts
@@ -1,16 +1,24 @@
-import {observable} from "mobx";
-
-export enum Screen {
-    'MainPage',
-    'Profile',
-    'Score',
-    'Status',
-    'ListGift',
-}
-
-
-export class ScreenStore{
-@observable screen:Screen = Screen.MainPage
-}
-
-export default new ScreenStore();
+// import {onPatch, types} from "mobx-state-tree";
+// import makeInspectable from "mobx-devtools-mst"; // Mobx State Tree dev tools
+//
+// export enum ScreenList {
+//     'MainPage',
+//     'Profile',
+//     'Score',
+//     'Status',
+//     'ListGift',
+// }
+//
+// export const ScreenStore = types.model({
+//     currentScreen: ScreenList.MainPage
+// }).actions(self => ({
+//     setScreen(screen) {
+//         self.currentScreen = screen;
+//     },
+// }))
+//
+// // Debugging tools
+// onPatch(ScreenStore, patch => {
+//     console.log(patch); // writes in console.log every changes in the state
+// });
+// makeInspectable(ScreenStore); // MST dev tools
--- src/app/stores/TodoStore.ts
+++ src/app/stores/TodoStore.ts
@@ -1,57 +1,18 @@
-import { observable, computed, action } from 'mobx';
-import { TodoModel } from 'app/models';
+import {types} from 'mobx-state-tree';
 
-export class TodoStore {
-  constructor(fixtures: TodoModel[]) {
-    this.todos = fixtures;
-  }
+const Todo = types.model({
+    title: types.string
+});
 
-  @observable public todos: Array<TodoModel>;
-
-  @computed
-  get activeTodos() {
-    return this.todos.filter((todo) => !todo.completed);
-  }
-
-  @computed
-  get completedTodos() {
-    return this.todos.filter((todo) => todo.completed);
-  }
-
-  @action
-  addTodo = (item: Partial<TodoModel>): void => {
-    this.todos.push(new TodoModel(item.text, item.completed));
-  };
-
-  @action
-  editTodo = (id: number, data: Partial<TodoModel>): void => {
-    this.todos = this.todos.map((todo) => {
-      if (todo.id === id) {
-        if (typeof data.completed == 'boolean') {
-          todo.completed = data.completed;
+const TodoStore = types
+    .model('TodoStore', {
+        todos: types.array(Todo)
+    })
+    .actions(self => ({
+        addTodo(title) {
+            self.todos.push({title});
         }
-        if (typeof data.text == 'string') {
-          todo.text = data.text;
-        }
-      }
-      return todo;
-    });
-  };
+    }));
 
-  @action
-  deleteTodo = (id: number): void => {
-    this.todos = this.todos.filter((todo) => todo.id !== id);
-  };
-
-  @action
-  completeAll = (): void => {
-    this.todos = this.todos.map((todo) => ({ ...todo, completed: true }));
-  };
-
-  @action
-  clearCompleted = (): void => {
-    this.todos = this.todos.filter((todo) => !todo.completed);
-  };
-}
 
 export default TodoStore;
